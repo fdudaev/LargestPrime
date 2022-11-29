a96f111210237dbf821f1697566ab8dd5ab4e1e2
fdudaev <61507529+fdudaev@users.noreply.github.com>
2022-11-29 09:33:49 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(28));
+
+    }
+    public static int getLargestPrime(int number) {
+        if (number < 2) {
+            return -1;
+        }
+        for (int i = number/2; i > 1; i--) {
+            if ((number % i) == 0) {
+                number = i;
+            }
+        }
+        return number;
+    }
+
+
+}
